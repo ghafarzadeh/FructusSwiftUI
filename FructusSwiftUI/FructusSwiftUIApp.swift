113ded9863e6593d0d2f2cb05025a0b7d80b39b0
ghafarzadeh <habib.ghafarzadeh@gmail.com>
2023-04-26 17:00:21 +0300

    init and add resource

---
+++ FructusSwiftUI/FructusSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusSwiftUIApp.swift
+//  FructusSwiftUI
+//
+//  Created by Habib Ghaffarzadeh on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
