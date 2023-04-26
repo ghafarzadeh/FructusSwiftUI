e823bbb5b0c539f3af88f282ecb5eaa5e17197f5
ghafarzadeh <habib.ghafarzadeh@gmail.com>
2023-04-26 17:44:28 +0300

    arrange folder

---
+++ FructusSwiftUI/View/FruitCardView.swift
@@ -0,0 +1,25 @@
+//
+//  FruitCardView.swift
+//  FructusSwiftUI
+//
+//  Created by Habib Ghaffarzadeh on 26.04.2023.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - REVIEW
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+    }
+}
