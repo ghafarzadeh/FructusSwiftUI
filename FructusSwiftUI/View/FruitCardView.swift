79799bcacc4f58cc63fb3a75bca8158f895ecc98
ghafarzadeh <habib.ghafarzadeh@gmail.com>
2023-05-02 09:51:10 +0300

    create FruitCardView and StartButtonView

--- FructusSwiftUI/View/FruitCardView.swift
+++ FructusSwiftUI/View/FruitCardView.swift
@@ -10,9 +10,47 @@ import SwiftUI
 struct FruitCardView: View {
     // MARK: - PROPERTIES
     
+    @State private var isAnimating: Bool = false
+    
     // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                
+                // FRUIT: TITLE
+                Text("BlueBerry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                // FRUIT: HEADLINE
+                Text("BlueBerry are sweet, BlueBerry are sweet, BlueBerry are sweet, BlueBerry are sweet, BlueBerry are sweet, BlueBerry are sweet, BlueBerry are sweet.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                // BUTTON: START
+                StartButtonView()
+            } //: VSTACK
+        } //: ZSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
     }
 }
 
@@ -21,5 +59,6 @@ struct FruitCardView: View {
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
         FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
     }
 }
